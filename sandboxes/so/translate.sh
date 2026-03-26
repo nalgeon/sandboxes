ea7fe1bf132301f33b1eb0f706dd475d08b1a31f
Anton <m@antonz.org>
2026-03-27 00:10:44 +0500

    so: translate command

--- sandboxes/so/translate.sh
+++ sandboxes/so/translate.sh
@@ -7,4 +7,9 @@ mkdir -p /tmp/sandbox
 cd /tmp/sandbox
 cp /sandbox/go.* ./
 cp /sandbox/src/* ./
-so translate .
+so translate -o generated .
+echo "-- main.h --"
+cat generated/main.h
+echo ""
+echo "-- main.c --"
+cat generated/main.c
